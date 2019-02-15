d25d322913c6d4b69da9a6befd3f0e6f90afad1b
wwx <593805475@qq.com>
2019-02-15 23:20:38 +0800

    add camera stream
    Signed-off-by: wwx <593805475@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 #include "res.h"
 
+using namespace std; 
 using namespace cv; 
 
 int main(int argc, char **argv)
 {
+	/*
 	Mat srcImage = imread(TJPG1);
-	imshow("t1", srcImage);
+	imshow("t1", srcImage);*/
 
+	/*
 	//--- 腐蚀 ---
-	/*Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
 	Mat dstImage;
 	erode(srcImage, dstImage, element);
 	imshow("t2", dstImage);*/
 
+	/*
 	//--- blur模糊 ---
-	/*Mat dstImage2; 
+	Mat dstImage2; 
 	blur(srcImage, dstImage2, Size(7, 7)); 
 	imshow("t3", dstImage2);*/
 
+	/*
 	//--- 边缘检测 ---
 	Mat dstImage3, edge, grayImage;	//参数定义
 	//创建与src同类型和大小的矩阵(dst)
@@ -34,8 +40,24 @@ int main(int argc, char **argv)
 	blur( grayImage, edge, Size(3,3));
 	//运行Canny算子
 	Canny(edge, edge, 3, 9, 3);
-	//
-	imshow("t4", edge);
+	imshow("t4", edge);*/
+  
+	//从摄像头读入视频
+	//sudo modprobe bcm2835-v4l2 
+	VideoCapture capture(0);
+	if(!capture.isOpened())
+	{
+			cout<<"camera open failure"<<endl;
+			return -1;
+	}
+	//循环显示每一帧
+	while(1)  
+	{  
+		Mat frame;  //定义一个Mat变量，用于存储每一帧的图像
+		capture>>frame;  //读取当前帧
+		imshow("读取视频",frame);  //显示当前帧
+		waitKey(30);  //延时30ms
+	}  
 
 	waitKey(0);
 }
